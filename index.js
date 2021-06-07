e2e66df5fa49558c058be2d53203bc3c8286276e
Cory <oneillco117@gmail.com>
2021-06-07 15:19:53 -0400

    commit tree ff8f12bd059d86f5fb9ad5037dd64937a5160e4c

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <= 400) 
+  return "This one is on me!"
+  else if(ride <= 2499)
+  return "I will gladly take your thirty bucks."
+  else(ride >= 2500)
+  return "No can do."
+
+} 
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return ternaryCheckCity = city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+switch (tipAmount) {
+  case 'generous':
+    return "Thank you so much.";
+    break;
+  case 'not as generous':
+    return "Thank you.";
+    break;
+    default: 
+    return 'Bye.';
+
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
 }
